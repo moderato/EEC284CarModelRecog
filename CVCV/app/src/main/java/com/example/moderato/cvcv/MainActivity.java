c10da599dac5f82a7acdfb18242fdbf1a6d0cbbb
Zhongyi LIN <moderato926@gmail.com>
2016-11-08 15:33:45 -0800

    Minor Changes

--- CVCV/app/src/main/java/com/example/moderato/cvcv/MainActivity.java
+++ CVCV/app/src/main/java/com/example/moderato/cvcv/MainActivity.java
@@ -33,15 +33,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
     };
 
-
-    static {
-//        if (!OpenCVLoader.initDebug()) {
-//            Log.d(TAG, "OpenCV not loaded");
-//        } else {
-//            Log.d(TAG, "OpenCV loaded！");
-//        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,13 +43,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         javaCameraView.setCvCameraViewListener(this);
     }
 
-//    /**
-//     * A native method that is implemented by the 'native-lib' native library,
-//     * which is packaged with this application.
-//     */
-//    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("opencv_java3");
         System.loadLibrary("OpencvNativeClass");
